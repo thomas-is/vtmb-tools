29f1f37af048ed9f3ca1d9a9804f06f52aa21336
thomas-is <thomas@0l.fi>
2022-12-22 20:24:04 +0100

    :bug: remove double quotes

--- include/pc.sh
+++ include/pc.sh
@@ -47,81 +47,81 @@ EOF
 
 brujahFemale() {
 cat << EOF
-"models/character/pc/female/brujah/armor0/brujah_female_armor_0.mdl"
-"models/character/pc/female/brujah/armor1/brujah_female_armor_1.mdl"
-"models/character/pc/female/brujah/armor2/brujah_female_armor_2.mdl"
-"models/character/pc/female/brujah/armor3/brujah_female_armor_3.mdl"
+models/character/pc/female/brujah/armor0/brujah_female_armor_0.mdl
+models/character/pc/female/brujah/armor1/brujah_female_armor_1.mdl
+models/character/pc/female/brujah/armor2/brujah_female_armor_2.mdl
+models/character/pc/female/brujah/armor3/brujah_female_armor_3.mdl
 EOF
 }
 
 gangrelFemale() {
 cat << EOF
-"models/character/pc/female/gangrel/armor0/Gangrel_female_Armor_0.mdl"
-"models/character/pc/female/gangrel/armor1/Gangrel_female_Armor_1.mdl"
-"models/character/pc/female/gangrel/armor2/Gangrel_female_Armor_2.mdl"
-"models/character/pc/female/gangrel/armor3/Gangrel_female_Armor_3.mdl"
+models/character/pc/female/gangrel/armor0/Gangrel_female_Armor_0.mdl
+models/character/pc/female/gangrel/armor1/Gangrel_female_Armor_1.mdl
+models/character/pc/female/gangrel/armor2/Gangrel_female_Armor_2.mdl
+models/character/pc/female/gangrel/armor3/Gangrel_female_Armor_3.mdl
 EOF
 }
 
 brujahMale() {
 cat << EOF
-"models/character/pc/male/brujah/armor0/brujah_male_armor_0.mdl"
-"models/character/pc/male/brujah/armor1/brujah_male_armor_1.mdl"
-"models/character/pc/male/brujah/armor2/brujah_male_armor_2.mdl"
-"models/character/pc/male/brujah/armor3/brujah_male_armor_3.mdl"
+models/character/pc/male/brujah/armor0/brujah_male_armor_0.mdl
+models/character/pc/male/brujah/armor1/brujah_male_armor_1.mdl
+models/character/pc/male/brujah/armor2/brujah_male_armor_2.mdl
+models/character/pc/male/brujah/armor3/brujah_male_armor_3.mdl
 EOF
 }
 
 ganrelMale() {
 cat << EOF
-"models/character/pc/male/gangrel/armor_0/Gangrel_Male_Armor_0.mdl"
-"models/character/pc/male/gangrel/armor_1/Gangrel_Male_Armor_1.mdl"
-"models/character/pc/male/gangrel/armor_2/Gangrel_Male_Armor_2.mdl"
-"models/character/pc/male/gangrel/armor_3/Gangrel_Male_Armor_3.mdl"
+models/character/pc/male/gangrel/armor_0/Gangrel_Male_Armor_0.mdl
+models/character/pc/male/gangrel/armor_1/Gangrel_Male_Armor_1.mdl
+models/character/pc/male/gangrel/armor_2/Gangrel_Male_Armor_2.mdl
+models/character/pc/male/gangrel/armor_3/Gangrel_Male_Armor_3.mdl
 EOF
 }
 
 malkavianMale() {
 cat << EOF
-"models/character/pc/male/malkavian/armor0/Malkavian_Male_Armor_0.mdl"
-"models/character/pc/male/malkavian/armor1/Malkavian_Male_Armor_1.mdl"
-"models/character/pc/male/malkavian/armor2/Malkavian_Male_Armor_2.mdl"
-"models/character/pc/male/malkavian/armor3/Malkavian_Male_Armor_3.mdl"
+models/character/pc/male/malkavian/armor0/Malkavian_Male_Armor_0.mdl
+models/character/pc/male/malkavian/armor1/Malkavian_Male_Armor_1.mdl
+models/character/pc/male/malkavian/armor2/Malkavian_Male_Armor_2.mdl
+models/character/pc/male/malkavian/armor3/Malkavian_Male_Armor_3.mdl
 EOF
 }
 
 nosferatuMale() {
 cat << EOF
-"models/character/pc/male/nosferatu/armor0/Nosferatu.mdl"
-"models/character/pc/male/nosferatu/armor1/Nosferatu_Male_armor_1.mdl"
-"models/character/pc/male/nosferatu/armor2/Nosferatu_Male_armor_2.mdl"
-"models/character/pc/male/nosferatu/armor3/Nosferatu_Male_armor_3.mdl"
+models/character/pc/male/nosferatu/armor0/Nosferatu.mdl
+models/character/pc/male/nosferatu/armor1/Nosferatu_Male_armor_1.mdl
+models/character/pc/male/nosferatu/armor2/Nosferatu_Male_armor_2.mdl
+models/character/pc/male/nosferatu/armor3/Nosferatu_Male_armor_3.mdl
 EOF
 }
 
 toreadorMale() {
 cat << EOF
-"models/character/pc/male/toreador/armor0/toreador_Male_Armor_0.mdl"
-"models/character/pc/male/toreador/armor1/toreador_Male_Armor_1.mdl"
-"models/character/pc/male/toreador/armor2/toreador_Male_Armor_2.mdl"
-"models/character/pc/male/toreador/armor3/toreador_Male_Armor_3.mdl"
+models/character/pc/male/toreador/armor0/toreador_Male_Armor_0.mdl
+models/character/pc/male/toreador/armor1/toreador_Male_Armor_1.mdl
+models/character/pc/male/toreador/armor2/toreador_Male_Armor_2.mdl
+models/character/pc/male/toreador/armor3/toreador_Male_Armor_3.mdl
 EOF
 }
 
 tremereMale() {
 cat << EOF
-"models/character/pc/male/tremere/armor0/tremere_Male_Armor_0.mdl"
-"models/character/pc/male/tremere/armor1/tremere_Male_Armor_1.mdl"
-"models/character/pc/male/tremere/armor2/tremere_Male_Armor_2.mdl"
-"models/character/pc/male/tremere/armor3/tremere_Male_Armor_3.mdl"
+models/character/pc/male/tremere/armor0/tremere_Male_Armor_0.mdl
+models/character/pc/male/tremere/armor1/tremere_Male_Armor_1.mdl
+models/character/pc/male/tremere/armor2/tremere_Male_Armor_2.mdl
+models/character/pc/male/tremere/armor3/tremere_Male_Armor_3.mdl
 EOF
 }
 
 ventrueMale() {
 cat << EOF
-"models/character/pc/male/ventrue/armor0/ventrue_Male_Armor_0.mdl"
-"models/character/pc/male/ventrue/armor1/ventrue_Male_Armor_1.mdl"
-"models/character/pc/male/ventrue/armor2/ventrue_Male_Armor_2.mdl"
-"models/character/pc/male/ventrue/armor3/ventrue_Male_Armor_3.mdl"
+models/character/pc/male/ventrue/armor0/ventrue_Male_Armor_0.mdl
+models/character/pc/male/ventrue/armor1/ventrue_Male_Armor_1.mdl
+models/character/pc/male/ventrue/armor2/ventrue_Male_Armor_2.mdl
+models/character/pc/male/ventrue/armor3/ventrue_Male_Armor_3.mdl
 EOF
 }
